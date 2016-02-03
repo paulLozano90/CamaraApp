b26053c68487bc1c74d638e3a1495112355af19c
paulLozano90 <plc90@hotmail.es>
2016-02-03 10:43:29 +0100

    CamaraApp

---
+++ CamaraApp/CamaraApp/Foto.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace CamaraApp
+{
+    public partial class Foto : ContentPage
+    {
+        public Foto()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var f = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+            {
+                Directory = "Fotos",
+                Name = "mifoto.jpg"
+            });
+
+            if (f != null)
+            {
+                MiFoto.Source = ImageSource.FromStream(() =>
+                {
+                    var st = f.GetStream();
+                    f.Dispose();
+                    return st;
+                });
+            }
+        }
+    }
+}
